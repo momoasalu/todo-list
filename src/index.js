cf45184c6b9072aa0b0b497ea3a9999d616b8d39
momoasalu <motunrayoasalu@gmail.com>
2023-08-25 10:32:24 +0100

    create application functions + modules

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-console.log('hello!')
+
+const ToDo = (title, description, dueDate, priority, project) => {
+    return {
+        title, 
+        description, 
+        dueDate, 
+        priority, 
+        project, 
+        completed
+    }
+}
+
+const Project = (name, todos) => {
+    return {
+        name,
+        todos
+    }
+}
+
+const List = (function () {
+    const toDos = [];
+    const projects = [];
+
+    return {
+        toDos,
+        projects,
+    }
+})()

e3bb7d12374d4b589d1158aa9df32e860154475a
momoasalu <motunrayoasalu@gmail.com>
2023-08-25 10:32:24 +0100

    create application functions + modules

--- main.js
+++ main.js
@@ -3,7 +3,34 @@ var __webpack_exports__ = {};
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-console.log('hello!')
+
+const ToDo = (title, description, dueDate, priority, project) => {
+    return {
+        title, 
+        description, 
+        dueDate, 
+        priority, 
+        project, 
+        completed
+    }
+}
+
+const Project = (name, todos) => {
+    return {
+        name,
+        todos
+    }
+}
+
+const List = (function () {
+    const toDos = [];
+    const projects = [];
+
+    return {
+        toDos,
+        projects,
+    }
+})()
 /******/ })()
 ;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLHFCIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdG9kby1saXN0Ly4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKCdoZWxsbyEnKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTs7QUFFQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7O0FBRUE7QUFDQTtBQUNBOztBQUVBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EsQ0FBQyxHIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdG9kby1saXN0Ly4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbIlxuY29uc3QgVG9EbyA9ICh0aXRsZSwgZGVzY3JpcHRpb24sIGR1ZURhdGUsIHByaW9yaXR5LCBwcm9qZWN0KSA9PiB7XG4gICAgcmV0dXJuIHtcbiAgICAgICAgdGl0bGUsIFxuICAgICAgICBkZXNjcmlwdGlvbiwgXG4gICAgICAgIGR1ZURhdGUsIFxuICAgICAgICBwcmlvcml0eSwgXG4gICAgICAgIHByb2plY3QsIFxuICAgICAgICBjb21wbGV0ZWRcbiAgICB9XG59XG5cbmNvbnN0IFByb2plY3QgPSAobmFtZSwgdG9kb3MpID0+IHtcbiAgICByZXR1cm4ge1xuICAgICAgICBuYW1lLFxuICAgICAgICB0b2Rvc1xuICAgIH1cbn1cblxuY29uc3QgTGlzdCA9IChmdW5jdGlvbiAoKSB7XG4gICAgY29uc3QgdG9Eb3MgPSBbXTtcbiAgICBjb25zdCBwcm9qZWN0cyA9IFtdO1xuXG4gICAgcmV0dXJuIHtcbiAgICAgICAgdG9Eb3MsXG4gICAgICAgIHByb2plY3RzLFxuICAgIH1cbn0pKCkiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=

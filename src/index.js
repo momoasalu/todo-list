6f686975fee51adfe51d5bf8db1a5f1ea671b9d4
momoasalu <motunrayoasalu@gmail.com>
2023-08-25 13:06:22 +0100

    add basic functions

--- src/index.js
+++ src/index.js
@@ -1,28 +1,96 @@
 
-const ToDo = (title, description, dueDate, priority, project) => {
+const ToDo = (title, description, dueDate, priority, project, completed, checklist) => {
+    /*const checklistItem = (name, checked) => {
+        const toggleComplete = {
+
+        }
+        return {name, checked}
+    }*/
+
+    const toggleComplete = function () {
+        completed = completed ? false : true;
+    }
+
+    const changePriority = function (newPriority) {
+        priority = newPriority;
+    }
+
+    //const addChecklistItem
+
     return {
         title, 
         description, 
         dueDate, 
+        checklist,
         priority, 
         project, 
-        completed
+        completed,
+        toggleComplete,
+        changePriority
     }
 }
 
-const Project = (name, todos) => {
+const Project = (name, toDos) => {
+    const addToProject = function (toDo) {
+        if (!toDos.includes(toDo)) {
+            toDos.push(toDo);
+            toDo.project = this;
+        }
+    }
+
+    const removeFromProject = function (toDo) {
+        if (toDos.includes(toDo)) {
+            toDo.project = null;
+            const index = toDos.findIndex(toDo);
+            toDos.splice(index, 1);
+        }
+    }
+
     return {
         name,
-        todos
+        toDos,
+        addToProject,
+        removeFromProject
     }
 }
 
 const List = (function () {
-    const toDos = [];
+    let toDos = [];
     const projects = [];
+
+    const createToDo = function (title, description, dueDate, priority, checklist = []) {
+        const newToDo = ToDo(title, description, dueDate, priority, null, false, checklist);
+        toDos.push(newToDo);
+        return newToDo;
+    }
+
+    const createProject = function (name) {
+        const newProject = Project(name, []);
+        projects.push(newProject);
+        return newProject;
+    }
+
+    const deleteTodo = function (toDo) {
+        const listIndex = toDos.findIndex(toDo);
+        toDos.splice(listIndex, 1);
+        if (toDo.project) {
+            const projectIndex = toDo.project.toDos.findIndex(toDo);
+            toDo.project.toDos.splice(projectIndex, 1);
+        }
+    }
+
+    const deleteProject = function (proj) {
+        toDos = toDos.filter((todo) => todo.project === proj);
+        projIndex = projects.indexOf(proj);
+        projects.splice(projIndex, 1);
+    }
 
     return {
         toDos,
         projects,
+        createToDo,
+        createProject,
+        deleteTodo,
+        deleteProject
     }
-})()
+})();
